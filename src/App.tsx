3d28b7c71e0296da771919896c4351ef6d2f26e0
Morga.Yeh <morga.ye@e-funtech.com>
2025-03-02 17:22:59 +0800

    [Tai] 調整 gh-page BaseUrl 與 github 路徑相同

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,35 @@ import DetailsPage from "./pages/DetailsPage";
 import ButtonListPage from "./pages/ButtonListPage";
 import RoundedPage from "./pages/RoundedPage";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+        {
+          path: "/detail",
+          element: <DetailsPage />,
+        },
+        {
+          path: "/button",
+          element: <ButtonListPage />,
+        },
+        {
+          path: "rounded",
+          element: <RoundedPage />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: "/detail",
-        element: <DetailsPage />,
-      },
-      {
-        path: "/button",
-        element: <ButtonListPage />,
-      },
-      {
-        path: "rounded",
-        element: <RoundedPage />,
-      },
-    ],
-  },
-]);
+    basename: "/tai-fex", // Set basename here
+  }
+);
 
 function App() {
   return <RouterProvider router={router} />;
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import viteBasicSslPlugin from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
+  base: "/tai-fex/",
   plugins: [react(), viteBasicSslPlugin()],
   server: {
     proxy: {
